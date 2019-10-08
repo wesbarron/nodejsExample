9b742490e75de51483d6bcc79b32cb8cce222554
JoseJCanedo <canedo_jose@columbusstate.edu>
2019-10-08 22:56:44 +0000

    node example

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.send("hello world");
+});
+
+app.get('/about', function(){
+    res.send("<h1>about page</h1>");
+})
+
+app.listen(port, function(){
+
+});
