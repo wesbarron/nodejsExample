ac2ff76fcc1c97ffa5f8a2d672aac84582dbb043
JoseJCanedo <canedo_jose@columbusstate.edu>
2019-10-15 22:27:23 +0000

    change to EJS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", 'ejs');
+
 app.get('/', function(req, res){
     res.send("hello world");
 });
