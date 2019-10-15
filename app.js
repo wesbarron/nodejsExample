3b6363358b9a50ec736fbed9371631b055d6aceb
JoseJCanedo <canedo_jose@columbusstate.edu>
2019-10-15 23:06:07 +0000

    add add task

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var http = require('http');
 var path =  require('path');
 const express = require("express");
+var bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ encoded: true}));
+
+var task = ["clean", "cook"];
 
 app.get('/', function(req, res){
     res.render("index");
 });
 
-app.get('/about', function(){
-    res.send("<h1>about page</h1>");
-})
+app.post('/addtask', function(req, res){
+    var newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect('/');
+});
 
 http.createServer(app).listen(port, function(){
 });
