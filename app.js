205babf6f98bf02e85a1b5fcc9f28ed8abd752c0
JoseJCanedo <canedo_jose@columbusstate.edu>
2019-10-17 22:13:50 +0000

    t

--- app.js
+++ app.js
@@ -10,14 +10,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ encoded: true}));
 
 var task = ["clean", "cook"];
+var complete = ["eat", "sleep"];
 
 app.get('/', function(req, res){
-    res.render("index", {task:task});
+    res.render("index", {task:task, complete:complete});
 });
 
 app.post('/addtask', function(req, res){
     var newTask = req.body.newtask;
     task.push(newTask);
+    res.redirect('/');
+});
+
+app.post('/removetask', function(req, res){
+
     res.redirect('/');
 });
 
