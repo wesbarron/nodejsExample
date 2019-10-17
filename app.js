dc4f576d21cb2ddac5f3ad0aa706de1b227008a6
JoseJCanedo <canedo_jose@columbusstate.edu>
2019-10-17 22:37:27 +0000

    t

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.post('/removetask', function(req, res){
         task.splice(task.indexOf(completeTask), 1);
     }else if (typeof completeTask === "object"){
         for(var i = 0; i < completeTask.length ; i++){
-            complete.push(completeTask);
-            task.splice(task.indexOf(completeTask), 1);
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
         }
     }
     res.redirect('/');
