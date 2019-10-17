cd57a570245867a8b6225e636cc2942af03ecb84
JoseJCanedo <canedo_jose@columbusstate.edu>
2019-10-17 22:56:03 +0000

    t

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ encoded: true}));
 
 var task = ["clean", "cook"];
