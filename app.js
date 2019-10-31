56a14752a6cec481e4a87debd4d8fdcb9a5b4785
JoseJCanedo <53786534+JoseJCanedo@users.noreply.github.com>
2019-10-31 17:34:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,91 @@
 var http = require('http');
 var path = require('path');
-var express = require('express');
+const express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
-var port = process.env.PORT || 3000;
+const app = express();
+var mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("view engine", 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-const fetch = require('node-fetch');
+app.use(bodyParser.urlencoded({encoded: false}));
+const Todo = require('./models/todo.model');
+const mongoDB = 'mongodb+srv://test_name:2823@cluster0-utawt.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error:"));
 
-var img = '';
-var year = '';
-var title = '';
-var total = 2220;
+var task = [];
+var complete = [];
 
-app.get("/", function(req, res) {
-    fetchComic(null);
-    res.render("index", { img: img, year: year, title: title, showButton: false});
-});
-
-app.get('/getRandom', function(req, res){
-  var rand = Math.floor((Math.random() * total) + 1);
-  fetchComic(rand);
-  res.render("index", { img: img, year: year, title: title, showButton: true});
-});
-
-app.get('/updateRandom', function(req, res){
-  var rand = Math.floor((Math.random() * total) + 1);
-  fetchComic(rand);
-  res.render("comicFrame", { img: img, year: year, title: title, showButton: true});
-});
-
-function fetchComic(rand) {
-  var url;
-  var current = false;
-  if(rand){
-    url = 'http://xkcd.com/' + rand + '/info.0.json';
-  }else{
-    current = true;
-    url = 'http://xkcd.com/info.0.json'
-  }
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-    img = data.img;
-    title = data.title;
-    year = data.year;
-    if(current){
-      total = data.num;
-    }
-    }).catch(err => {
-      res.redirect('/error');
+app.get('/', function(req, res){
+    Todo.find(function(err, todo){
+        if(err){
+            console.log(err);
+        }else{
+            task = [];
+            complete = [];
+            for(i = 0; i<todo.length; i++){
+                if(todo[i].done){
+                    complete.push(todo[i].item);
+                }else{
+                task.push(todo[i].item);
+                }
+            }
+        }
     });
-}
+    res.render("index", {task:task, complete:complete});
+});
+
+app.post('/addtask', function(req, res){
+    let newTodo = new Todo({
+        item: req.body.newtask,
+        done: false
+    });
+    newTodo.save(function(err){
+        if (err){
+            console.log(err);
+        }
+        res.redirect('/');
+    });
+});
+
+app.post('/removetask', function(req, res){
+    var completeTask = req.body.check;
+    if(typeof completeTask === "string"){
+        Todo.updateOne({item: completeTask},{done: true}, function(err){
+            console.log(err);
+        });
+    }else if (typeof completeTask === "object"){
+        for(var i = 0; i < completeTask.length; i++){
+            Todo.updateOne({item: completeTask[i]},{done: true}, function(err){
+            console.log(err);
+        });
+        }
+    }
+    res.redirect('/');
+});
+
+app.post("/deleteTodo", function(req, res){
+    var deleteTask = req.body.delete;
+    if(typeof deleteTask === "string"){
+        Todo.deleteOne({item: deleteTask}, function(err){
+            console.log(err);
+        });
+    }else if (typeof deleteTask === "object"){
+        for(var i = 0; i < deleteTask.length; i++){
+            Todo.deleteOne({item: deleteTask}, function(err){
+            console.log(err);
+        });
+        }
+    }
+
+    res.redirect('/');
+});
 
 http.createServer(app).listen(port, function(){
-   console.log('example app' + port);
 });
+
