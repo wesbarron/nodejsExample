e2297bcdf773ddc2dc8dc0ad7f24bbb2ef1a7654
wesbarron <barron_wesley@columbusstate.edu>
2019-11-07 23:43:04 +0000

    w

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,24 @@
 import React, {Component} from 'react';
 
 class Home extends Component{
+    state = {
+        data:null
+    }
 
+    componentDidMount(){
+        this.callBackEnd();
+    }
+
+    callBackEnd = async () => {
+        try{
+            const resp = await fetch('/api');
+            const respJson = await resp.json();
+            console.log(respJson);
+            this.setState({data:respJson});
+        }catch{
+            console.log(err);
+        }
+    }
     //write the function to do fetch
 
     render(){
